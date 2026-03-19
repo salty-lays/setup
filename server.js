4ea80d7ae87a7df9eb019c46f1375d69e644f0c0
salty-lays <chetanpatil21444@gmail.com>
2026-03-19 15:34:35 +0530

    json fixed

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 
 const app = express();
-app.use(express.text());
+app.use(express.json());
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
