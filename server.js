1d6b30f63ea15b2667b4ec9f1707bb57f44593aa
salty-lays <chetanpatil21444@gmail.com>
2026-03-19 11:14:47 +0530

    kk

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 app.use(express.text());
 
-app.post('/', (req, res) => {
-    console.log('Received text:', req.body);
-    res.send('Text received');
+// Store latest received text
+let latestText = "No data received yet";
+
+// Serve HTML
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-const PORT = process.env.PORT || 3000;
+// Endpoint to get latest text
+app.get('/data', (req, res) => {
+    res.send(latestText);
+});
 
+// Receive POST
+app.post('/', (req, res) => {
+    latestText = req.body;
+});
+
+const PORT = 3000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
 });
